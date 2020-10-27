b7f59e82c2e8b262f57e22a3642c29aa52c3c8aa
Ubuntu <ubuntu@ip-172-31-34-88.ec2.internal>
2020-10-27 00:50:57 +0000

    atualizacao

--- Atividade04.py
+++ Atividade04.py
@@ -1,16 +1,29 @@
 import rospy
+
 from std_msgs.msg import String
 
+rospy.init_node('no_soma')
 
-rospy.init_node('teste1')
 
+numero_matricula = String()
+
+def sub_somaCallBack(msg):
+    global numero_matricula
+    numero_matricula = msg
+
+    
 def timerCallBack(event):
-        msg = String()
-        msg.data = '34784'
-        pub.publish(msg)
-        
-        
-pub = rospy.Publisher('/topic1', String, queue_size=1)
+    soma = 0
+    for k in range(len(numero_matricula.data)):
+        soma = soma+int(numero_matricula.data[k]);
+ 
+    msg = String()
+    msg.data = str(soma)
+    pub_soma.publish(msg)
+ 
+
+sub_soma = rospy.Subscriber('/no_matricula/matricula', String, sub_somaCallBack)
+pub_soma = rospy.Publisher('/no_soma/soma', String, queue_size=1)
 timer = rospy.Timer(rospy.Duration(0.1), timerCallBack)
 
-rospy.spin()
+rospy.spin() 
